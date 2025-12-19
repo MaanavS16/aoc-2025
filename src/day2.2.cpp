0061eecb7a1a151adb96f56275f5de43ca131ea5
Maanav Singh <Maanavsingh1234@gmail.com>
2025-12-19 15:01:26 -0500

    add day2.2

---
+++ src/day2.2.cpp
@@ -0,0 +1,77 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <ranges>
+#include <utility>
+
+auto solve(std::vector<std::pair<int64_t, int64_t>> const &input) {
+
+  long long res = 0;
+
+  const auto lil_elf_bro_involved = [](auto const id) {
+    std::size_t const len = std::ceil(std::log10(id));
+
+    for (auto period = 1u; period <= len / 2; period++) {
+      auto ms_id = id;
+
+      for (std::size_t i = 0; i < len - period; ++i) {
+        ms_id /= 10;
+      }
+
+      uint64_t expected = 0;
+      for (std::size_t i = 0; i < len / period; ++i) {
+        expected += ms_id * std::pow(10, period * i);
+      }
+      if (expected == id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (auto const &[r1, r2] : input) {
+    for (std::size_t n = r1; n <= r2; ++n) {
+      if (lil_elf_bro_involved(n)) {
+        res += n;
+      }
+    }
+  }
+  return res;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::vector<std::pair<int64_t, int64_t>> lines{};
+  std::string line;
+  std::cin >> line;
+
+  auto line_view = std::string_view(line);
+
+  for (auto const &rng : (line_view | std::views::split(','))) {
+    auto subrange = rng | std::views::split('-');
+    auto it = subrange.begin();
+    std::pair<int64_t, int64_t> rs;
+
+    auto first_begin = std::ranges::begin(*it);
+    auto first_end = std::ranges::end(*it);
+    std::from_chars(first_begin, first_end, rs.first);
+    ++it;
+    auto second_begin = std::ranges::begin(*it);
+    auto second_end = std::ranges::end(*it);
+    std::from_chars(second_begin, second_end, rs.second);
+
+    lines.push_back(rs);
+  }
+
+  auto const start = std::chrono::steady_clock::now();
+  auto const ans = solve(lines);
+  auto const end = std::chrono::steady_clock::now();
+  std::cout << "ans: " << ans << '\n';
+  std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end -
+                                                                     start)
+                   .count()
+            << "us\n";
+  return 0;
+}
